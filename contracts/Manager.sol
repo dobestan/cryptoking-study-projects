fb53dc64d8c193e0d5b22a4db2b3747ce41962cd
Suchan An <dobestan@gmail.com>
2022-07-31 12:53:35 +0900

    Refactor Manager contract via Epochs Library

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -3,49 +3,28 @@ pragma solidity >=0.8.0;
 
 
 import "./UtilityToken.sol";
+import "./utils/Epochs.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 contract Manager {
-    using Counters for Counters.Counter;
+    using Epochs for Epochs.Epochs;
 
-    struct Epoch {
-        address tokenContractAddress;
+    Epochs.Epochs private _epochs;
+
+    function epochId() public view returns (uint) {
+        return _epochs.id();
     }
 
-    Counters.Counter private _epochId;
-    Epoch[] private _epochs;
-
-    event EpochCreated(uint EpochId, address tokenContractAddress);
-
-    function epochId() public view virtual returns (uint) {
-        return _epochId.current();
+    function epoch() public view returns (Epoch.Epoch memory) {
+        return _epochs.current();
     }
 
-    /// @dev Return current epoch.
-    function epoch() public view virtual returns (Epoch memory) {
-        require(_epochId.current() > 0, "Epoch: not a single epoch is created.");
-        return _epochs[_epochId.current() - 1];
-    }
-
-    function getEpoch(uint epochId_) public view virtual returns (Epoch memory) {
-        return _epochs[epochId_ - 1];
-    }
-
-    /// @dev Create a new epoch.
     function createEpoch(uint _initialSupply) public returns (uint) {
-        _epochId.increment();
-        
-        // 1. create a new ERC20 UtilityToken.
-        UtilityToken token = new UtilityToken(_initialSupply);
-        address tokenContractAddress = address(token);
-        
-        // 2. create a new Epoch and append to _epochs array.
-        Epoch memory createdEpoch = Epoch(tokenContractAddress);
-        _epochs.push(createdEpoch);
-        
-        emit EpochCreated(_epochId.current(), tokenContractAddress);       
-        
-        return _epochId.current();
+        return _epochs.create(_initialSupply);
+    }
+
+    function getEpoch(uint _epochId) public view returns (Epoch.Epoch memory) {
+        return _epochs.get(_epochId);
     }
 }
---
+++ contracts/utils/Epochs.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "../UtilityToken.sol";
+
+
+library Epoch {  
+    struct Epoch {
+        address tokenContractAddress;
+    }
+}
+
+
+library Epochs {
+    using Counters for Counters.Counter;
+
+    struct Epochs {
+        Counters.Counter _epochId;
+        Epoch.Epoch[] _epochs;
+    }
+
+    event EpochCreated(uint EpochId, address tokenContractAddress);
+
+    function id(Epochs storage self) public view returns (uint) {
+        require(self._epochId.current() > 0, "Epochs: not a single epoch is created.");
+        return self._epochId.current();
+    }
+
+    function current(Epochs storage self) internal view returns (Epoch.Epoch storage) {
+        require(self._epochId.current() > 0, "Epochs: not a single epoch is created.");
+        return self._epochs[self._epochId.current() - 1];
+    }
+
+    function get(Epochs storage self, uint epochId_) internal view returns (Epoch.Epoch storage) {
+        return self._epochs[epochId_ - 1];
+    }
+
+    function create(Epochs storage self, uint _initialSupply) internal returns (uint) {
+        self._epochId.increment();
+
+        // 1. create a new ERC20 UtilityToken.
+        UtilityToken token = new UtilityToken(_initialSupply);
+        address tokenContractAddress = address(token);
+        
+        // 2. create a new Epoch and append to _epochs array.
+        Epoch.Epoch memory createdEpoch = Epoch.Epoch(tokenContractAddress);
+        self._epochs.push(createdEpoch);
+        
+        emit EpochCreated(self._epochId.current(), tokenContractAddress);       
+        
+        return self._epochId.current();
+    }
+}
