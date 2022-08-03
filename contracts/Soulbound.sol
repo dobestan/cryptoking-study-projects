00c89b23226ebbced04db5d44f8428fa575eb98d
Suchan An <dobestan@gmail.com>
2022-08-03 20:51:22 +0900

    Update Soulbound: add Access Control and Transferable Whitelist features

--- contracts/Soulbound.sol
+++ contracts/Soulbound.sol
@@ -3,18 +3,39 @@ pragma solidity >=0.8.0;
 
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract Soulbound is ERC1155 {
+contract Soulbound is ERC1155, Ownable {
     uint public constant REGISTERED = 0;
-    // TODO: REGISTERED Token cannot be transfer(soulbound).
+    // uint public constant SOMETHING_TRANSFERABLE = 1;
+
+    mapping(uint => bool) public isTransferable;
 
     constructor() ERC1155("") {
-
+        // Transferable Whitelist
+        // isTransferable[SOMETHING_TRANSFERABLE] = true;
     }
 
-    // TODO: Access Control(Ownable).
-    function register(address soul) public {
+    function setTransferable(uint _tokenId, bool transferable) public onlyOwner {
+        isTransferable[_tokenId] = transferable;
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint[] memory ids,
+        uint[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        // _beforeTokenTransfer called on 
+        // _safeTransferFrom, _safeBatchTransferFrom, _mint, _mintBatch, _burn, _burnBatch.
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        require(msg.sender == owner() || isTransferable[ids[0]], "Soulbound: Token is non-transferable.");
+    }
+
+    function register(address soul) public onlyOwner {
         require(balanceOf(soul, REGISTERED) == 0, "Soulbound: only fresh soul can register.");
         _mint(soul, REGISTERED, 1, "");
     }
